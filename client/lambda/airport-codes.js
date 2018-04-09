ab5fad7fa7bb5bce67483ee23c244677b496180f
Piyush <piyushaggarwal.incedo@gmail.com>
2018-04-09 14:06:24 +0530

    skill commit changes

--- client/lambda/airport-codes.js
+++
@@ -1,9 +0,0 @@
-var config = require('./config');
-
-
-module.exports.getCodeFromCity = function(myCity){
- for(var i=0; i< config.airpotCodesList.length;i++){
- 	if(config.airpotCodesList[i].cityName.toLowerCase() === myCity.toLowerCase())
- 	  return config.airpotCodesList[i].airportCode;
- }
-}
